827c39ff014b741bba56a686f3f63cacd7ca216a
Justin McLellan <justin.mclellan99@gmail.com>
2022-02-25 09:08:08 -0600

    logout refactor and testing

---
+++ src/app/components/modals/logout/logout-modal.component.spec.ts
@@ -0,0 +1,80 @@
+import { ComponentFixture, discardPeriodicTasks, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
+import { DynamicDialogModule, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { Subscription } from 'rxjs';
+import { LogoutModalComponent } from './logout-modal.component';
+
+let refMock: any;
+
+describe('LogoutModalComponent', () => {
+  let component: LogoutModalComponent;
+  let fixture: ComponentFixture<LogoutModalComponent>;
+
+  beforeEach(
+    waitForAsync(() => {
+      refMock = {
+        close: jest.fn(),
+      };
+      TestBed.configureTestingModule({
+        imports: [DynamicDialogModule],
+        declarations: [LogoutModalComponent],
+        providers: [{ provide: DynamicDialogRef, useValue: refMock }],
+      }).compileComponents();
+    }),
+  );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LogoutModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should set counter to 60 seconds', () => {
+    expect(component.counter).toEqual(60);
+  });
+
+  describe('ngOnInit', () => {
+    it('should call onTimerInterval every second', fakeAsync(() => {
+      component.onTimerInterval = jest.fn();
+      fixture.detectChanges();
+      component.ngOnInit();
+      tick(1500);
+      expect(component.onTimerInterval).toBeCalledTimes(1);
+      discardPeriodicTasks();
+    }));
+  });
+
+  describe('ngOnDestroy', () => {
+    it('should call unsubscribe on timerSub', () => {
+      component.timerSub = {
+        unsubscribe: jest.fn(),
+      } as unknown as Subscription;
+      component.ngOnDestroy();
+      expect(component.timerSub.unsubscribe).toBeCalledTimes(1);
+    });
+  });
+
+  describe('logout', () => {
+    it('should call close on ref with true', () => {
+      component.logout();
+      expect(refMock.close).toHaveBeenNthCalledWith(1, true);
+    });
+  });
+
+  describe('onTimerInterval', () => {
+    it('should decrement counter if counter is greater than 1', () => {
+      component.counter = 5;
+      component.onTimerInterval();
+      expect(component.counter).toEqual(4);
+    });
+    it('should call logout if counter is less than or equal to 1', () => {
+      component.counter = 1;
+      component.logout = jest.fn();
+      component.onTimerInterval();
+      expect(component.logout).toBeCalledTimes(1);
+    });
+  });
+});
--- src/app/components/modals/logout/logout-modal.component.ts
+++ src/app/components/modals/logout/logout-modal.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
-import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription, tap } from 'rxjs';
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
 
 @Component({
   selector: 'app-logout-modal',
   templateUrl: './logout-modal.component.html',
 })
-export class LogoutModalComponent implements OnDestroy {
-  public counter: number = 60; // Log out after this many seconds
+export class LogoutModalComponent implements OnInit, OnDestroy {
+  counter: number = 60; // Log out after this many seconds
   // Create a timer obserable that counts down
-  public logoutTimer$: Subscription = interval(1000).subscribe(() => {
+  logoutTimer$ = interval(1000).pipe(
+    tap(() => {
+      this.onTimerInterval();
+    }),
+  );
+  timerSub!: Subscription;
+
+  constructor(public ref: DynamicDialogRef) {}
+
+  ngOnInit(): void {
+    this.timerSub = this.logoutTimer$.subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.timerSub?.unsubscribe();
+  }
+
+  /** Log the user out manually */
+  logout(): void {
+    this.ref.close(true);
+  }
+
+  onTimerInterval(): void {
     // If timer is greater than 0, count down.
     if (this.counter > 1) {
       this.counter--;
@@ -17,16 +39,5 @@ export class LogoutModalComponent implements OnDestroy {
       // If timer hits zero or below, CLOSE this modal which toggles the logout action in AuthService
       this.logout();
     }
-  });
-
-  constructor(public dialogService: DialogService, public ref: DynamicDialogRef) {}
-
-  public ngOnDestroy() {
-    this.logoutTimer$?.unsubscribe();
-  }
-
-  /** Log the user out manually */
-  public logout() {
-    this.ref.close(true);
   }
 }
