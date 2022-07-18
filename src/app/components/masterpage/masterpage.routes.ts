8e77bc7f411d2c72a9312bcafe2606019bb7a6b8
Jerrol Krause <JerrolKrause@users.noreply.github.com>
2022-07-18 10:15:01 -0400

    Clean up masterpage handling

--- src/app/components/masterpage/masterpage.routes.ts
+++
@@ -1,7 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { MasterPageModule } from './masterpage.module';
-
-const routes: Routes = [];
-
-export const routing: ModuleWithProviders<MasterPageModule> = RouterModule.forChild(routes);
