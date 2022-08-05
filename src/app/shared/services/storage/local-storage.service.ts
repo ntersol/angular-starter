f2058d580eea0b62c8ba021c7ad1985eb6242563
Jerrol Krause <JerrolKrause@users.noreply.github.com>
2022-08-05 15:49:15 -0400

    Update documentation

--- src/app/shared/services/storage/local-storage.service.ts
+++ src/app/shared/services/storage/local-storage.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { StorageService } from './base-storage.service';
 
+/**
+ * An abstraction layer for local storage that is SSR compatible and supports Observables
+ */
 @Injectable({
   providedIn: 'root',
 })
--- src/app/shared/services/storage/session-storage.service.ts
+++ src/app/shared/services/storage/session-storage.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { StorageService } from './base-storage.service';
 
+/**
+ * An abstraction layer for session storage that is SSR compatible and supports Observables
+ */
 @Injectable({
   providedIn: 'root',
 })
