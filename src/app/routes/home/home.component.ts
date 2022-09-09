98b498902172e49b030247cb5d6e411be813f917
Jerrol Krause <JerrolKrause@users.noreply.github.com>
2022-09-09 11:30:12 -0400

    Deprecate settings service in favor of appstorage

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -8,19 +8,9 @@ import { AppStorageService } from '$shared';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  constructor(private storage: AppStorageService) {}
+  constructor() {}
 
-  ngOnInit() {
-    // this.localStorage.getItem$('user', { isJson: true }).subscribe(x => console.warn(x));
-    const token = this.storage.token;
-    const user = this.storage.user;
-
-    this.storage.token = 'test';
-    this.storage.user = { name: 'test', id: 1234, username: 'test2' };
-
-    this.storage.user$.subscribe(x => console.log(x));
-    // const name = user?.name;
-  }
+  ngOnInit() {}
 
   ngOnDestroy() {}
 }
