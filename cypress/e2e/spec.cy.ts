70e6d4d169b169dda900195b4cc48c1d86abfb2a
Jerrol <jerrolkrause@gmail.com>
2024-02-05 10:10:53 -0500

    Initial Cypress implementation

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,7 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running!')
-  })
-})
+    cy.visit('/');
+    cy.contains('Write cool code');
+    cy.contains('Sidebar stuff');
+  });
+});
